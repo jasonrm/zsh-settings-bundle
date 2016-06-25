341f26ee6886dc6d31825b8f90b69157c03b7e65
Jason R. McNeil <jason@jasonrm.net>
2016-06-24 21:18:11 -0700

    Don't include forward slash in WORDCHARS

--- 99-zsh.zsh
+++ 99-zsh.zsh
@@ -1,2 +1,9 @@
 #/bin/zsh
 setopt no_share_history
+
+backward-delete-to-slash () {
+  local WORDCHARS=${WORDCHARS//\//}
+  zle .backward-delete-word
+}
+zle -N backward-delete-to-slash
+bindkey '^w' backward-delete-to-slash
