4b3bd90e7996f07d8e1d7c76320e8cd7e4e32f32
Jason R. McNeil <jason@jasonrm.net>
2016-09-17 02:33:22 -0700

    Check if php-version is installed

--- 99-php.zsh
+++ 99-php.zsh
@@ -6,6 +6,6 @@ if [[ ! -f ${HOME}/bin/composer ]]; then
     chmod 755 ${HOME}/bin/composer
 fi
 
-if type "brew" > /dev/null 2>&1; then
+if type "brew" > /dev/null 2>&1 && brew --prefix php-version > /dev/null 2>&1; then
   source $(brew --prefix php-version)/php-version.sh && php-version 7.0 || php-version 5.6
 fi
