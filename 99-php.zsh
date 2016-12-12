c818c0900de7666241c63fc9dc5383c6c0ab7fb4
Jason R. McNeil <jason@jasonrm.net>
2016-12-12 12:37:11 -0700

    Use latest PHP 7

--- 99-php.zsh
+++ 99-php.zsh
@@ -8,5 +8,5 @@ if [[ ! -f ${HOME}/bin/composer ]]; then
 fi
 
 if type "brew" > /dev/null 2>&1 && brew --prefix php-version > /dev/null 2>&1; then
-  source $(brew --prefix php-version)/php-version.sh && php-version 7.0 || php-version 5.6
+  source $(brew --prefix php-version)/php-version.sh && php-version 7
 fi
