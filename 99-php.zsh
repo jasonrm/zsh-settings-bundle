8bb191c9952bcf2878c42c4a349c354adcb455eb
Jason R. McNeil <jason@jasonrm.net>
2019-02-19 10:49:30 -0700

    No longer using php-version

--- 99-php.zsh
+++ 99-php.zsh
@@ -3,10 +3,7 @@ export PATH="$HOME/.composer/vendor/bin/:$PATH"
 
 mkdir -p ${HOME}/bin
 if [[ ! -f ${HOME}/bin/composer ]]; then
+	echo "installing composer"
     curl https://getcomposer.org/composer.phar -o ${HOME}/bin/composer
     chmod 755 ${HOME}/bin/composer
-fi
-
-if type "brew" > /dev/null 2>&1 && brew --prefix php-version > /dev/null 2>&1; then
-  source $(brew --prefix php-version)/php-version.sh && php-version 7
 fi
